fb07b5ed0a48c96218301db49e1ca0a23726fb73
hli30 <harrylll93@gmail.com>
2018-04-03 20:32:40 +0000

    implemented product detail page feature test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Clicking on details link shows product details" do
+    visit root_path
+
+    page.first('.actions').find_link("Details").click
+
+    save_screenshot
+
+    expect(page).to have_css '.products-show'
+
+  end 
+end
